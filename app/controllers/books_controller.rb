90053a4a0a70702a879f0a71caaea8b7df32e436
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-12-03 18:41:48 +0000

    erorr favo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,7 @@ class BooksController < ApplicationController
     @books = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
