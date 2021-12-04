85908576fbc8d824eb7bcff40a930937c2cc5a2b
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-12-04 05:38:03 +0000

    comment favorite addition

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id:book.id)
+    favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id:book.id)
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book.id)
+    redirect_back(fallback_location: root_path)
   end
 end
