8015a28c692fd8ca3b649039cac314174dc72da7
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-12-09 20:18:38 +0000

    Application issues　6 finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
     render :book_comments
   end
 
   def destroy
-    BookComment.find_by(id: params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     render :book_comments
   end
 
   private
-
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
